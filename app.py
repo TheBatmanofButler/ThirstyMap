dae954be6bc8bc498375fd934cd800c62be671d7
Ganesh Ravichandran <gr2483@columbia.edu>
2016-08-21 19:26:48 -0400

    wtf heroku

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import os
 
 app = Flask(__name__)
 
-port = int(os.environ.get('PORT', 5000))
-
 @app.route("/")
 def hello():
     return "Hello Worlds!"
 
 if __name__ == "__main__":
+	port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
